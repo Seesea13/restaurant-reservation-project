1a061255f6817b8acf9025c5d6402c5705ab32d9
Seesea13 <su1337780799@gmail.com>
2025-05-14 20:32:35 +0300

    Update auth.js

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
-const authMiddleware = require('../middleware/authMiddleware'); // ✅ Verify JWT
+const authMiddleware = require('../middleware/authMiddleware'); 
 const bcrypt = require('bcrypt');
 const db = require('../models'); // Used to access User model
 
